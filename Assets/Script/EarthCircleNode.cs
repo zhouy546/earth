14c2ec34ecf697d7a67543f158e20315cf2b2e7b
zhouy546 <zhouyunqi546@gmail.com>
2018-07-20 10:31:49 +0800

    add leantween plugin, and initization earth;

---
+++ Assets/Script/EarthCircleNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthCircleNode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Initializtion() {
+        LeanTween.scale(this.gameObject, this.transform.localScale * 1.2f, .5f).setLoopPingPong();
+
+        SetPosition();
+
+        transform.LookAt(EarthCircleNodesCtr.instance.sphereTransform);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetPosition() {
+        Vector3 temp = (this.transform.position - EarthCircleNodesCtr.instance.sphereTransform.position).normalized;
+
+       this.transform.position = EarthCircleNodesCtr.instance.distance*temp + EarthCircleNodesCtr.instance.sphereTransform.position ;
+    }
+}
---
+++ Assets/Script/EarthCircleNodesCtr.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EarthCircleNodesCtr : MonoBehaviour {
+   public float distance;
+   public static EarthCircleNodesCtr instance;
+   public Transform sphereTransform;
+   public List<EarthCircleNode> earthCircleNodes = new List<EarthCircleNode>();
+	// Use this for initialization
+	void Start () {
+        Initialization();
+
+    }
+
+    public void Initialization() {
+
+        if (instance == null) {
+
+            instance = this;
+
+        }
+
+        sphereTransform = this.GetComponent<Transform>();
+
+        earthCircleNodes = this.GetComponentsInChildren<EarthCircleNode>().ToList();
+
+
+        foreach (var item in earthCircleNodes)
+        {
+            item.Initializtion();
+        }
+        RotateSelf();
+    }
+    LTDescr RotateLTDescr;
+    public void RotateSelf() {
+        RotateLTDescr= LeanTween.rotateAround(this.gameObject, Vector3.up, -360f, 50f).setOnComplete(delegate ()
+        {
+            RotateSelf();
+
+            RotateLTDescr = null;
+        });
+    }
+
+    public void cancelRotatel() {
+        LeanTween.cancel(RotateLTDescr.id);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
