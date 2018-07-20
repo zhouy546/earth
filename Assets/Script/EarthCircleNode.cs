aacfee8a6f8987176cc73196acbbb1f7d2e2329b
zhouy546 <zhouyunqi546@gmail.com>
2018-07-20 16:25:23 +0800

    zoom in Function

--- Assets/Script/EarthCircleNode.cs
+++ Assets/Script/EarthCircleNode.cs
@@ -27,4 +27,71 @@ public class EarthCircleNode : MonoBehaviour {
 
        this.transform.position = EarthCircleNodesCtr.instance.distance*temp + EarthCircleNodesCtr.instance.sphereTransform.position ;
     }
+
+    public void MoveToCamera() {
+
+        EarthCircleNodesCtr.instance.cancelRotatel();
+
+        Vector3 SphereMeshRot = new Vector3(0, EarthCircleNodesCtr.instance.sphereRotateAngle.y + MeasureYrot(), 0);
+       
+        EarthCircleNodesCtr.instance.RotateSphereMesh(SphereMeshRot);
+
+
+        Vector3 XRot = new Vector3(EarthCircleNodesCtr.instance.XRotTransform.localRotation.eulerAngles.x+MeasureXrot(), 0, 0);
+       
+        EarthCircleNodesCtr.instance.RotateXRot(XRot);
+    }
+
+    private float MeasureYrot() {
+
+        Vector2 NodeXZPos = new Vector2(this.transform.position.x, transform.position.z);
+
+        Vector2 SphereXZPos = new Vector2(EarthCircleNodesCtr.instance.sphereTransform.position.x, EarthCircleNodesCtr.instance.sphereTransform.position.z);
+
+        float topDis = (NodeXZPos - SphereXZPos).magnitude;
+
+        float temp = Mathf.Abs( transform.position.z - EarthCircleNodesCtr.instance.sphereTransform.position.z);
+
+        float rotY =Mathf.Rad2Deg*Mathf.Acos(temp / topDis);
+
+        if (transform.position.x > SphereXZPos.x && transform.position.z < SphereXZPos.y)
+        {
+            return rotY;
+        }
+        else if (transform.position.x > SphereXZPos.x && transform.position.z > SphereXZPos.y)
+        {
+            return (90-rotY + 90);
+        }
+        else if (transform.position.x < SphereXZPos.x && transform.position.z < SphereXZPos.y)
+        {
+            return -rotY;
+        }
+        else if (transform.position.x < SphereXZPos.x && transform.position.z > SphereXZPos.y)
+        {
+            return -(90-rotY + 90);
+        }
+
+        return 0;
+    }
+
+    private float MeasureXrot() {
+
+        Vector3 NodePos = this.transform.position;
+
+        Vector3 SpherePos = EarthCircleNodesCtr.instance.sphereTransform.position;
+
+        float Ldis = (NodePos - SpherePos).magnitude;
+
+        float Ydis = Mathf.Abs(NodePos.y - SpherePos.y);
+
+        float rotX = Mathf.Rad2Deg * Mathf.Asin(Ydis / Ldis);
+
+        if (transform.position.y > SpherePos.y)
+        {
+            return -rotX;
+        } else if (transform.position.y < SpherePos.y) {
+            return rotX;
+        }
+        return 0;
+    }
 }
--- Assets/Script/EarthCircleNodesCtr.cs
+++ Assets/Script/EarthCircleNodesCtr.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class EarthCircleNodesCtr : MonoBehaviour {
-   public float distance;
-   public static EarthCircleNodesCtr instance;
-   public Transform sphereTransform;
-   public List<EarthCircleNode> earthCircleNodes = new List<EarthCircleNode>();
+
+    public float time;
+    public float distance;
+    public static EarthCircleNodesCtr instance;
+    public Transform sphereTransform;
+    public Vector3 sphereRotateAngle;
+    public Transform XRotTransform;
+    public List<EarthCircleNode> earthCircleNodes = new List<EarthCircleNode>();
 	// Use this for initialization
 	void Start () {
         Initialization();
@@ -22,7 +27,7 @@ public class EarthCircleNodesCtr : MonoBehaviour {
 
         }
 
-        sphereTransform = this.GetComponent<Transform>();
+        XRotTransform = this.GetComponent<Transform>();
 
         earthCircleNodes = this.GetComponentsInChildren<EarthCircleNode>().ToList();
 
@@ -32,26 +37,59 @@ public class EarthCircleNodesCtr : MonoBehaviour {
             item.Initializtion();
         }
 
-        RotateSelf();
+        RotateAroundsphere();
     }
 
     LTDescr RotateLTDescr;
-
-    public void RotateSelf() {
-        RotateLTDescr= LeanTween.rotateAround(this.gameObject, Vector3.up, -360f, 50f).setOnComplete(delegate ()
+    public void RotateAroundsphere() {
+        RotateLTDescr= LeanTween.rotateAround(sphereTransform.gameObject, Vector3.up, -360f, time).setOnUpdate(delegate (float value) {
+            sphereRotateAngle =new Vector3(0, sphereTransform.eulerAngles.y,0);
+        }).setOnComplete(delegate ()
         {
-            RotateSelf();
+            RotateAroundsphere();
 
             RotateLTDescr = null;
         });
     }
 
+    public void RotateSphereMesh(Vector3 Angel) {
+        LeanTween.rotateLocal(sphereTransform.gameObject, Angel, 1f);
+    }
+
+    public void RotateXRot(Vector3 Angel, Action action=null) {
+        LeanTween.rotateLocal(XRotTransform.gameObject, Angel, 1f).setOnComplete(delegate () {
+            if (action != null) {
+                action();
+            }
+        });
+
+    }
+
+    public void ResetBack()
+    {
+       // RotateSphereMesh(sphereRotateAngle);
+        RotateXRot(Vector3.zero, RotateAroundsphere);
+
+    }
+
     public void cancelRotatel() {
+        if (RotateLTDescr != null)
         LeanTween.cancel(RotateLTDescr.id);
     }
 
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            foreach (var item in earthCircleNodes)
+            {
+                if (item.name == "test") {
+                    item.MoveToCamera();
+                }
+            }
+        }
 
-	}
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            ResetBack();
+
+    }
 }
